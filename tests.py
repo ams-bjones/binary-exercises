31e810eb61c917c32164428bdde573e4bb1cb855
Brian Jones <bjones@ashmanorschool.com>
2014-03-14 17:22:48 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 #here we write our tests
 import unittests
+import binary as bin
 
 class tests(unittest.TestCase):
     def test1(self):
-       pass
+       bin.number = 1
+       assert bin.number = 1
 
 if __name == "__main__":
     unittest.main()
